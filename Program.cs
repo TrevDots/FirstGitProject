2e679045c13809600320c9c07deb13f205ae4f51
Trevor <dotson.trevor@gmail.com>
2019-10-14 22:11:32 -0600

    committing all changes without calling git add

--- Program.cs
+++ Program.cs
@@ -13,6 +13,14 @@ namespace MyNewProject
 
             aFriend = Console.ReadLine();
             Console.WriteLine($"Hello {aFriend}");
+
+            string firstFriend = "Maria";
+            string secondFriend = "Sage";
+
+            Console.WriteLine ($"My friends are {firstFriend} and {secondFriend}");
+            Console.WriteLine ($"The name {firstFriend} has {firstFriend.Length} letters.");
+            Console.WriteLine ($"The name {secondFriend} has {secondFriend.Length} letters.");
+
         }
     }
 }
